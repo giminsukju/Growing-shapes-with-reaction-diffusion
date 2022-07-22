c41ffed4b5b9d51b964ec1db0a5de89aab058911
giminsukju <giminsukju@gmail.com>
2022-07-23 06:45:15 +0900

    2D grid added

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,3 @@
-#include "polyscope/polyscope.h"
-
 #include <igl/PI.h>
 #include <igl/avg_edge_length.h>
 #include <igl/barycenter.h>
@@ -13,13 +11,18 @@
 #include <igl/readOBJ.h>
 #include <igl/cotmatrix.h>
 #include <igl/massmatrix.h>
+#include <igl/loop.h>
+#include <igl/writeOBJ.h>
 
 
 #include<Eigen/SparseCholesky>	
 
+
+#include "polyscope/polyscope.h"
 #include "polyscope/messages.h"
 #include "polyscope/point_cloud.h"
 #include "polyscope/surface_mesh.h"
+
 
 #include <iostream>
 #include <unordered_set>
@@ -36,6 +39,33 @@ int iVertexSource = 7;
 int n; // # of vertices in the mesh
 Eigen::VectorXd randN;
 
+void function(int n, int m, Eigen::MatrixXd& func) {
+    for (int i = 0; i < (n); i++) {
+        for (int j = 0; j < (m); j++) {
+            func(i, j) = (m + 1) * i + j;
+        }
+    }
+}
+
+void create2DGridManual(int n, int m, Eigen::MatrixXd &V, Eigen::MatrixXi &F) {
+    using namespace Eigen;
+    V.resize((n+1)*(m+1), 3);
+    F.resize(n*m, 4);
+
+    for (int i = 0; i < (n + 1); i++) {
+        for (int j = 0; j < (m + 1); j++) {
+            V.row((m + 1) * i + j) = Vector3d(-j, -i, 0);
+        }
+    }
+
+    int idx = 0;
+    for (int i = 0; i < (n); i++) {
+        for (int j = 0; j < (m); j++) {
+            F.row(idx) = Vector4i((m + 1) * i + j, (m + 1) * (i + 1) + j, (m + 1) * (i + 1) + j + 1, (m + 1) * i + j + 1);
+            idx += 1;
+        }
+    }
+}
 
 // Semi-Implicit 2
 void reactionDiffusionImplicit(float t, float _alpha, float _beta, float _s, float _da, float _db) {
@@ -45,15 +75,24 @@ void reactionDiffusionImplicit(float t, float _alpha, float _beta, float _s, flo
     SparseMatrix<double> L;
     igl::cotmatrix(meshV, meshF, L); // returns V by V
 
-    VectorXd a = VectorXd::Constant(n, 1, 4) + randN; // initial a
-    VectorXd b = VectorXd::Constant(n, 1, 4) + randN; // initial b
+    VectorXd a = VectorXd::Constant(n, 1, 4); // initial a
+    VectorXd b = VectorXd::Constant(n, 1, 4); // initial b
 
-    VectorXd alpha = VectorXd::Constant(n, 1, 12);  // decay rate of a
-    VectorXd beta = VectorXd::Constant(n, 1, 16); // growing rate of b
+    //Sanderson et al
+    VectorXd alpha = VectorXd::Constant(n, 1, 12) + randN;  // decay rate of a
+    VectorXd beta = VectorXd::Constant(n, 1, 16) + randN; // growing rate of b
     float s = (float)1 / 128; // reaction rate
-    float da = (float)1 / 16; // diffusion rate of as
-    float db = (float)1 / 4; // diffusion raate of b
+    float da = (float)0.14; // diffusion rate of as
+    float db = (float)0.04; // diffusion raate of b
     float deltat = 0.1; // time step
+
+    //Sliders
+    //VectorXd alpha = VectorXd::Constant(n, 1, _alpha);  // decay rate of a
+    //VectorXd beta = VectorXd::Constant(n, 1, _beta); // growing rate of b
+    //float s = _s; // reaction rate
+    //float da = _da; // diffusion rate of as
+    //float db = _db; // diffusion raate of b
+    //float deltat = 0.5; // time step
 
     SparseMatrix<double> Y(n, n), Z(n, n), b_diag(n, n), a_diag(n, n), I(n, n); I.setIdentity();
 
@@ -82,7 +121,6 @@ void reactionDiffusionImplicit(float t, float _alpha, float _beta, float _s, flo
     //temp->setMapRange({ 3.3,4.7 });
 }
 
-
 void reactionDiffusionExplicit(float t, float _alpha, float _beta, float _s, float _da, float _db) {
     using namespace Eigen;
     using namespace std;
@@ -97,7 +135,7 @@ void reactionDiffusionExplicit(float t, float _alpha, float _beta, float _s, flo
     VectorXd a = VectorXd::Constant(n, 1, 4) + randN;
     VectorXd b = VectorXd::Constant(n, 1, 4) + randN;
 
-    //Converge to Turing's pattern
+    //Sanderson et al
     VectorXd alpha = VectorXd::Constant(n, 1, 12);  // decay rate of a
     VectorXd beta = VectorXd::Constant(n, 1, 16); // growing rate of b
     float s = (float)1/128; // reaction rate
@@ -170,7 +208,6 @@ void computeCotangentLaplacian(float t) {
   auto temp = mesh->addVertexScalarQuantity("laplacian", A);
   temp -> setMapRange({-0.1, 0.1});
 }
-
 
 void addCurvatureScalar() {
   using namespace Eigen;
@@ -285,7 +322,6 @@ void callback() {
 
   }
 
-
   ImGui::SameLine();
   ImGui::InputInt("source vertex", &iVertexSource);
 
@@ -293,6 +329,12 @@ void callback() {
 }
 
 int main(int argc, char **argv) {
+  //         //
+  // 2D grid //
+  //         //
+  Eigen::MatrixXd grid_V;
+  Eigen::MatrixXi grid_F;
+  create2DGridManual(10, 20, grid_V, grid_F);
 
   // Options
   polyscope::options::autocenterStructures = true;
@@ -302,23 +344,47 @@ int main(int argc, char **argv) {
   // Initialize polyscope
   polyscope::init();
 
-  std::string filename = "../spot.obj";
-  std::cout << "loading: " << filename << std::endl;
+  // Register the mesh with Polyscope
+  polyscope::registerSurfaceMesh("input mesh", grid_V, grid_F);
+
+  Eigen::MatrixXd func(10, 20);
+  function(10, 20, func);
+  auto mesh = polyscope::getSurfaceMesh("input mesh");
+
+  func.transposeInPlace();
+  Eigen::VectorXd func_F(Eigen::Map<Eigen::VectorXd>(func.data(), func.cols() * func.rows()));
+  auto temp = mesh->addFaceScalarQuantity("test", func_F);
+
+  polyscope::show();
+
+  //         //
+  // 3D mesh //
+  //         //
+  
+  //std::string filename = "../spot.obj";
+  //std::cout << "loading: " << filename << std::endl;
+
+  //Eigen::MatrixXd origV;
+  //Eigen::MatrixXi origF;
 
   // Read the mesh
-  igl::readOBJ(filename, meshV, meshF);
+  //igl::readOBJ(filename, origV, origF);
+  ////Eigen::SparseMatrix<double> S;
+  //igl::loop(origV.rows(), origF, S, meshF);
+  //meshV = S * origV;
 
   // Register the mesh with Polyscope
-  polyscope::registerSurfaceMesh("input mesh", meshV, meshF);
+  //polyscope::registerSurfaceMesh("input mesh", meshV, meshF);
 
-  n = meshV.rows();// number of vertex in the mesh
-  randN = Eigen::VectorXd::Random(n, 1);
+  //n = meshV.rows();// number of vertex in the mesh
+  //randN = Eigen::VectorXd::Random(n, 1);
 
   // Add the callback
-  polyscope::state::userCallback = callback;
+  // polyscope::state::userCallback = callback;
 
   // Show the gui
-  polyscope::show();
+  //polyscope::show();
+
 
   return 0;
 }
