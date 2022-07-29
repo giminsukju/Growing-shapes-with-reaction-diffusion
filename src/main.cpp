960b7d7cbe8ec464497a233e60fe418cf2232a63
giminsukju <giminsukju@gmail.com>
2022-07-30 04:31:23 +0900

    added morphogen switcher, initial morphogen modification based on mesh object size, progress indicator

--- src/main.cpp
+++ src/main.cpp
@@ -21,6 +21,7 @@
 #include <igl/is_boundary_edge.h>
 #include <igl/edge_flaps.h>
 #include <igl/edge_topology.h>
+#include <igl/bounding_box_diagonal.h>
 
 
 #include<Eigen/SparseCholesky>	
@@ -41,7 +42,8 @@
 // The mesh, Eigen representation
 Eigen::MatrixXd meshV;
 Eigen::MatrixXi meshF;
-Eigen::VectorXd original_scaling;
+Eigen::VectorXd original_scaling_0;
+Eigen::VectorXd original_scaling_1;
 
 // Options for algorithms
 int iVertexSource = 7;
@@ -254,10 +256,12 @@ void implicitReactionDiffusionGrayScott(float numSteps, float timeStep, double F
     float du = 1; // diffusion rate
     float dv = (float)0.5; // diffusion rate
 
-    // Modify V partially (eg ink drop in water) to drive diffusion
-    int reference_idx = rand() % (meshV.rows()+1); //random index between 0 and # rows in meshV (ie # of vertices)
-    // float desired_dist = 0.1; // for spot (about 1/20 of its size)
-    float desired_dist = 1; // for grid (about 1/20 of its size)
+    // Modify V partially (ie ink drop in water) to drive diffusion
+    int reference_idx = rand() % (meshV.rows()+1); //randomly pick one index between 0 and # rows in meshV (ie # of vertices)
+    double mesh_size = igl::bounding_box_diagonal(meshV); //largest EUclidean distance in the given mesh
+    double desired_dist = mesh_size / 20;
+    std::cout << mesh_size << "  " << desired_dist << std::endl;
+
     for (int i = 0; i < meshV.rows(); i++) {
         // when the Euclidean distance bw the reference idx vertex position & ith vertex < 0.1
         if ((pow(( meshV(i, 0) - meshV(reference_idx, 0)),2) + \
@@ -287,9 +291,12 @@ void implicitReactionDiffusionGrayScott(float numSteps, float timeStep, double F
         V = solver2.solve((V + timeStep * (UVV - kV - FV)).eval());
     }
 
-    original_scaling = U; //update scaling as a global variable
+    original_scaling_0 = U; //update scaling as a global variable
+    original_scaling_1 = V;
     auto temp = polyscope::getSurfaceMesh("input mesh");
-    auto mesh = temp->addVertexScalarQuantity("Scott Implicit", U);
+    auto mesh_0 = temp->addVertexScalarQuantity("morphogen 0", U);
+    auto mesh_1 = temp->addVertexScalarQuantity("morphogen 1", V);
+
 }
 
 void procruste(Eigen::MatrixXi F_init, Eigen::MatrixXd V_init, Eigen::MatrixXd V_rest, Eigen::MatrixXd& V_update) {
@@ -369,12 +376,13 @@ void constructDiamondRestMesh(Eigen::MatrixXd V_init, Eigen::MatrixXi F_init, Ei
         int face_1_idx = EF(i, 0);
         int face_2_idx = EF(i, 1);
 
-        int edge_v_1_idx = E(i, 0);
-        int edge_v_2_idx = E(i, 1);
-
-        int face_1_corner_v_idx = F_init(face_1_idx, EI(i, 0));
-        int face_2_corner_v_idx = F_init(face_2_idx, EI(i, 1));
         if ((face_1_idx != -1) && (face_2_idx != -1)) { // is inner edge
+            int edge_v_1_idx = E(i, 0);
+            int edge_v_2_idx = E(i, 1);
+
+            int face_1_corner_v_idx = F_init(face_1_idx, EI(i, 0));
+            int face_2_corner_v_idx = F_init(face_2_idx, EI(i, 1));
+
             // construct a flattened diamond that loks like:  
             //              
             //           edge_v_1_idx (v2)
@@ -410,30 +418,20 @@ void constructDiamondRestMesh(Eigen::MatrixXd V_init, Eigen::MatrixXi F_init, Ei
 
             F_rest_diamond.row(inner_edge_ctr) = Eigen::Vector4i(inner_edge_ctr * 4, inner_edge_ctr * 4 + 2, inner_edge_ctr * 4 + 1, inner_edge_ctr * 4 + 3); // v0 -> v2 -> v1 -> v3 order
             F_rest_diamond_in_org_v_idx.row(inner_edge_ctr) = Eigen::Vector4i(edge_v_1_idx, face_1_corner_v_idx, edge_v_2_idx, face_2_corner_v_idx); // v0 -> v2 -> v1 -> v3 order
-            //if (inner_edge_ctr == 200) { //TODO: debugging purposes, have to update declarations
-            //    Eigen::MatrixXd V_rest_diamond_1(1 * 4, 3);
-            //    Eigen::MatrixXi F_rest_diamond_1(1 * 2, 3);
-            //    V_rest_diamond_1.row(0) = Eigen::Vector3d(0.0, 0.0, 0.0);
-            //    V_rest_diamond_1.row(1) = Eigen::Vector3d(flat_v1_x, flat_v1_y, 0.0);
-            //    V_rest_diamond_1.row(2) = Eigen::Vector3d(flat_v2_x, flat_v2_y, 0.0);
-            //    V_rest_diamond_1.row(3) = Eigen::Vector3d(flat_v3_x, flat_v3_y, 0.0);
-
-            //    F_rest_diamond_1.row(0) = Eigen::Vector4i(0, 2, 1, 3);
-
-            //    polyscope::registerSurfaceMesh("diamond rest mesh edge 200", V_rest_diamond_1, F_rest_diamond_1);
-            //}
+            
             inner_edge_ctr += 1;
         }
     }
 }
 
-void growShape(int n_iteration, double alpha, double beta, double min_scaling, double max_scaling) { //TODO: take mesh file as an input
-    std::cout << n_iteration << " " << alpha << "  " << beta << " " << min_scaling << " " << max_scaling << std::endl;
+void growShape(int n_iteration, double alpha, double beta, double min_scaling, double max_scaling, int morphogen) { //TODO: take mesh file as an input
+    std::cout << "shape growing parameters: " << n_iteration << " " << alpha << " " << beta << " " << min_scaling << " " << max_scaling << std::endl;
 
     Eigen::MatrixXd V_init = meshV;
     Eigen::MatrixXi F_init = meshF;
 
-    // set non-uniform scaling, inner area scaling > outer area scaling
+    // manual scaling
+    // grid: set non-uniform scaling, inner area scaling > outer area scaling
     //Eigen::VectorXd scaling = Eigen::VectorXd::Constant(V_init.rows(), 1, 1.2);
     //int m = 10; //TODO: take input mesh size as a variable
     //for (int i = 5; i <= 15; i++) { // row
@@ -442,7 +440,7 @@ void growShape(int n_iteration, double alpha, double beta, double min_scaling, d
     //    }
     //}
 
-    // set non-uniform scaling, spot face scaling > spot body scaling
+    // spot: set non-uniform scaling, spot face scaling > spot body scaling
     //Eigen::VectorXd scaling = Eigen::VectorXd::Constant(V_init.rows(), 1, 1.2);
     //Eigen::VectorXd reference = V_init.row(64); //roughly the farthest point from spot's body, spot's head starting point
     //float head_dist = 0.66; //roughly Euclidean distance bw the reference point and the boundary of spot's body and head
@@ -454,23 +452,27 @@ void growShape(int n_iteration, double alpha, double beta, double min_scaling, d
     //    }
     //}
 
-    Eigen::VectorXd scaling  = ((max_scaling - min_scaling) * original_scaling) + Eigen::VectorXd::Constant(V_init.rows(), 1, min_scaling);
+    // scaling based on reaction-diffusion 
+    Eigen::VectorXd scaling(V_init.rows(), 1);
+    if (morphogen == 0) {
+        scaling = ((max_scaling - min_scaling) * original_scaling_0) + Eigen::VectorXd::Constant(V_init.rows(), 1, min_scaling);
+    }
+    else {
+        scaling = ((max_scaling - min_scaling) * original_scaling_1) + Eigen::VectorXd::Constant(V_init.rows(), 1, min_scaling);
+    }
 
     // construct rest mesh
     int n_faces = F_init.rows();
     Eigen::MatrixXd V_rest(n_faces * 3, 3); // position of n_faces * 3 vertices, ordered according to F_rest face
     Eigen::MatrixXi F_rest(n_faces, 3);
     constructRestMesh(V_init, F_init, scaling, V_rest, F_rest);
-    //polyscope::registerSurfaceMesh("triangle rest mesh", V_rest, F_rest);
 
     for (int itr_ctr = 0; itr_ctr < n_iteration; itr_ctr++) {
         // stretching 
         Eigen::MatrixXd V_stretched(V_init.rows(), 3);
         procruste(F_init, V_init, V_rest, V_stretched); //update V_stretched mesh
         V_init = V_init + (V_stretched - V_init) * alpha;
-        //if (itr_ctr == 0) { //debugging purposes, TODO: use f-string instead of multiple if statements
-        //    polyscope::registerSurfaceMesh("stretched: iter 1", V_init, F_init);
-        //}
+
         // construct diamond rest mesh for bending-resisting
         Eigen::MatrixXi E; //E(e) row = (vertex i, vertex j) edge
         Eigen::VectorXi EMAP;
@@ -486,17 +488,15 @@ void growShape(int n_iteration, double alpha, double beta, double min_scaling, d
         Eigen::MatrixXi F_rest_diamond(n_inner_edges, 4); // vertex indices of a diamond consist of (2*i)th triangle and (2*i+1)th triangl
         Eigen::MatrixXi F_rest_diamond_in_org_v_idx(n_inner_edges, 4); // vertex indices of a diamond consist of (2*i)th triangle and (2*i+1)th triangl
         constructDiamondRestMesh(V_init, F_init, V_rest_diamond, F_rest_diamond, F_rest_diamond_in_org_v_idx);
-        //if (itr_ctr == 0) {
-        //    polyscope::registerSurfaceMesh("diamond rest mesh: iter 1", V_rest_diamond, F_rest_diamond);
-        //}
 
         //bending resisting
         Eigen::MatrixXd V_bending_resisted(V_init.rows(), 3);
         procruste_diamond(F_rest_diamond_in_org_v_idx, V_init, V_rest_diamond, V_bending_resisted); //update V_bending_resisted mesh
         V_init = V_init + (V_bending_resisted - V_init) * beta;
-        //if (itr_ctr == 0) { //debugging purposes, TODO: use f-string instead of multiple if statements
-        //    polyscope::registerSurfaceMesh("bending resisted: iter 1", V_init, F_init);
-        //}
+
+        if (itr_ctr % 500) {
+            std::cout << "running iteration: " << itr_ctr << std::endl;//progress indication
+        }
     }
     polyscope::registerSurfaceMesh("final mesh", V_init, F_init);
 }
@@ -524,36 +524,29 @@ void callback() {
     static double max_scaling = 2.5; //scaling results from r-d would be scaled accordingly
     ImGui::InputDouble("Maximum scaling", &max_scaling);
 
+    static int morphogen;
+    ImGui::SliderInt("switch morphogens", &morphogen, 0, 1);
+
     static double alpha = 1.0;
     ImGui::InputDouble("alpha", &alpha);
 
     static double beta = 0.5;
     ImGui::InputDouble("beta", &beta);
 
-    //static int growing_max_n_iteration = 100;
-    //ImGui::InputInt("max iter", &growing_max_n_iteration);
-
     static int growing_n_iteration = 50;
     ImGui::InputInt("Growing iterations", &growing_n_iteration);
 
     if (ImGui::Button("growing shapes refresh")) {
-        growShape(growing_n_iteration, alpha, beta, min_scaling, max_scaling);
+        growShape(growing_n_iteration, alpha, beta, min_scaling, max_scaling, morphogen);
     }
-
-
-    //static int growing_n_iteration = 0;
-    //if ((ImGui::SliderInt("growing shapes", &growing_n_iteration, 0, growing_max_n_iteration)) ||
-    //    (ImGui::Button("growing shapes refresh"))) {
-    //    growShape(growing_n_iteration, alpha, beta, min_scaling, max_scaling);
-    //}
 }
 
 
 int main(int argc, char **argv) {
     polyscope::init();
 
-    //std::string filename = "../spot.obj";
-    std::string filename = "../original grid.obj";
+    std::string filename = "../spot.obj";
+    //std::string filename = "../original grid.obj";
 
     Eigen::MatrixXd origV, meshV1;
     Eigen::MatrixXi origF, meshF1;
